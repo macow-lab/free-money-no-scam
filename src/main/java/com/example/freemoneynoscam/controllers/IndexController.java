bc283b174316991b1043f8f0844326e8d4cf81c9
macow-lab <mmacovv@gmail.com>
2021-10-19 14:35:23 +0200

    Progress

--- src/main/java/com/example/freemoneynoscam/controllers/IndexController.java
+++ src/main/java/com/example/freemoneynoscam/controllers/IndexController.java
@@ -1,5 +1,8 @@
 package com.example.freemoneynoscam.controllers;
 
+import com.example.freemoneynoscam.services.EmailValidator;
+import com.example.freemoneynoscam.services.Database;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.util.MultiValueMap;
 
@@ -9,28 +12,48 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class IndexController {
+
+    EmailValidator validator = new EmailValidator();
+    Database database = new Database();
+
     @GetMapping("/")
-    public String index(){
+    public String index() {
         return "index";
     }
 
-	@PostMapping("/contact")
-	public String postContact(@RequestParam MultiValueMap body ) {
-		System.out.print(body);
-	    String result = "";
+    @PostMapping("/")
+    public String postContact(@RequestParam MultiValueMap body) {
+        database.setConnection();
+
+        String result = "fail";
+
         String input = String.valueOf(body.get("email"));
         System.out.println(input);
 
-
-            // Handle redirect
-            if (result.equals("400")) {
-                return "fail";
+        // Validate email
+        if (validator.validate(input)) {
+            if(database.saveEmail(input) == 200) {
+                result = "success";
+            } else {
+                result = "error";
             }
+        } else {
+            result = "fail";
+        }
 
-            if (result.equals("200")) {
-                return "success";
-            }
-            
-            return "contact";
-		} 
-	}
+        // Handle redirect
+        if (result == "fail") {
+            return "fail";
+        }
+
+        if (result == "success") {
+            return "success";
+        }
+
+        if (result == "error") {
+            return "error";
+        }
+
+        return "index";
+    }
+}
