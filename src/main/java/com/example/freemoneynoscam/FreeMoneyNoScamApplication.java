f75d071af15d1f8d779d9680b91c1ed1d2cfc752
JensC <jenschristian0002@gmail.com>
2021-10-19 14:09:59 +0200

    udvidet på database ting

--- src/main/java/com/example/freemoneynoscam/FreeMoneyNoScamApplication.java
+++ src/main/java/com/example/freemoneynoscam/FreeMoneyNoScamApplication.java
@@ -9,18 +9,6 @@ public class FreeMoneyNoScamApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(FreeMoneyNoScamApplication.class, args);
-        try{
-            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/easymoneysniper","root","password");
-            PreparedStatement psts = conn.prepareStatement("SELECT * from employees");
-            ResultSet resultSet = psts.executeQuery();
-            while(resultSet.next()){
-                System.out.println(resultSet.getString(2));
-            }
-
-        }catch(SQLException e){
-            System.out.println("Cannot connect to database");
-            e.printStackTrace();
-        }
     }
 
 }
---
+++ src/main/java/com/example/freemoneynoscam/services/Database.java
@@ -0,0 +1,36 @@
+package com.example.freemoneynoscam.services;
+import java.sql.*;
+
+public class Database {
+
+    public Database() {
+    }
+
+    private Connection connection = null;
+    public boolean setConnection() {
+        final String url = "jdbc:mysql://localhost:3306/easymoneysniper?serverTimezone=UTC";
+        boolean res = false;
+        try {
+            connection = DriverManager.getConnection(url, "root", "im;1337%HUMAN");
+            res = true;
+        } catch (SQLException ioerr) {
+            System.out.println(ioerr);
+        }
+        return res;
+    }
+
+    public int saveEmail(String email) {
+        String insstr = "INSERT INTO email(email) values (?)";
+        PreparedStatement preparedStatement;
+        try {
+            preparedStatement = connection.prepareStatement(insstr);
+            preparedStatement.setString(1, email);
+            preparedStatement.executeUpdate();
+        } catch (SQLException err) {
+            System.out.println("bad happened:" + err.getMessage());
+            return 400;
+        }
+        System.out.println("good happened");
+        return 200;
+    }
+}
