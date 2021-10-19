341ffab2d49315fa84155a7f7f83273f7c27160c
macow-lab <mmacovv@gmail.com>
2021-10-19 12:54:59 +0200

    Initial file creation

--- src/main/java/com/example/freemoneynoscam/FreeMoneyNoScamApplication.java
+++ src/main/java/com/example/freemoneynoscam/FreeMoneyNoScamApplication.java
@@ -2,12 +2,25 @@ package com.example.freemoneynoscam;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.sql.*;
 
 @SpringBootApplication
 public class FreeMoneyNoScamApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(FreeMoneyNoScamApplication.class, args);
+        try{
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/easymoneysniper","root","password");
+            PreparedStatement psts = conn.prepareStatement("SELECT * from employees");
+            ResultSet resultSet = psts.executeQuery();
+            while(resultSet.next()){
+                System.out.println(resultSet.getString(2));
+            }
+
+        }catch(SQLException e){
+            System.out.println("Cannot connect to database");
+            e.printStackTrace();
+        }
     }
 
 }
---
+++ src/main/java/com/example/freemoneynoscam/services/EmailValidator.java
@@ -0,0 +1,5 @@
+package com.example.freemoneynoscam.services;
+
+public class EmailValidator {
+    
+}
