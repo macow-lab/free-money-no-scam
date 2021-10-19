eaae81beb05be5e6c47a1fe7b0357cc37245c90d
macow-lab <mmacovv@gmail.com>
2021-10-19 13:51:12 +0200

    brr

--- src/main/java/com/example/freemoneynoscam/controllers/IndexController.java
+++ src/main/java/com/example/freemoneynoscam/controllers/IndexController.java
@@ -1,7 +1,11 @@
 package com.example.freemoneynoscam.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.util.MultiValueMap;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class IndexController {
@@ -9,4 +13,24 @@ public class IndexController {
     public String index(){
         return "index";
     }
-}
+
+	@PostMapping("/contact")
+	public String postContact(@RequestParam MultiValueMap body ) {
+		System.out.print(body);
+	    String result = "";
+        String input = String.valueOf(body.get("email"));
+        System.out.println(input);
+
+
+            // Handle redirect
+            if (result.equals("400")) {
+                return "fail";
+            }
+
+            if (result.equals("200")) {
+                return "success";
+            }
+            
+            return "contact";
+		} 
+	}
--- src/main/java/com/example/freemoneynoscam/services/EmailValidator.java
+++ src/main/java/com/example/freemoneynoscam/services/EmailValidator.java
@@ -1,5 +1,18 @@
 package com.example.freemoneynoscam.services;
 
 public class EmailValidator {
-    
+    // Method takes String and validates if it is correct email format.
+    public boolean validate(String email) {
+
+        boolean status = false;
+
+        if (email.contains("@") && email.contains(".")) {
+            status = true;
+        }
+        return status;
+
+        // Should be done with Regex to properly check if it is email format.
+        // This returns true on "@."
+
+    }
 }
